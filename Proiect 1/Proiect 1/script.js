04cd284fd2fc4817788170fa571b50ed9cc958bd
Ion Petrascu <ionpetrascuc0318@gmal.com>
2023-10-24 16:56:10 +0300

    add cookies message

--- Proiect 1/Proiect 1/script.js
+++ Proiect 1/Proiect 1/script.js
@@ -26,3 +26,30 @@ btnMobile.addEventListener("click",()=>{
         headerWrapper.style.flexDirection = "row";
     }
 })
+
+AOS.init();
+
+
+
+
+   
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+let cookiecook = getCookie("cookiecook"),
+cookiewin = document.getElementsByClassName('cookie_notice')[0];    
+if (cookiecook != "no") {
+ 
+    cookiewin.style.display="block"; 
+    
+    document.getElementById("cookie_close").addEventListener("click", function(){
+        cookiewin.style.display="none";    
+       
+        let date = new Date;
+        date.setDate(date.getDate() + 1);    
+        document.cookie = "cookiecook=no; path=/; expires=" + date.toUTCString();               
+    });
+}
